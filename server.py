68eac8c5db7c8f7a2d906396afbf88532b081140
andre <andre.larrosa.c@gmail.com>
2022-03-29 16:43:41 -0300

    espera n passos, os bombeiros aparecem e termina a simulacao

--- server.py
+++ server.py
@@ -1,38 +1,115 @@
-from mesa.visualization.modules import CanvasGrid, ChartModule, PieChartModule
-from mesa.visualization.ModularVisualization import ModularServer
-from mesa.visualization.UserParam import UserSettableParameter
+from pickle import FALSE, TRUE
+from mesa import Model
+from mesa.datacollection import DataCollector
+from mesa.space import Grid
+from mesa.time import RandomActivation
 
-from .model import ForestFire
+import csv
 
-COLORS = {"Fine": "#00AA00", "On Fire": "#880000", "Burned Out": "#000000"}
+from .agent import TreeCell
+
+def shield_trees_index(model): # arrumar para nao contar a mesma arvore mais de uma vez
+    """
+    Se uma arvore esta "Fine", mas sua vizinha "Burned Out", entao temos
+    que a arvore sobreviveu ao fogodireto devido a resitencia da casca.
+    Qual a porcentagem de arvores que impediram o avanco do fogo
+    """
+
+    count = 0
+    for agent in model.schedule.agents:
+        if agent.condition == "Fine":
+            flag = TRUE
+            for neighbor in agent.model.grid.neighbor_iter(agent.pos):
+                if neighbor.condition == "Burned Out" and flag == TRUE:
+                    count += 1
+                    flag = FALSE    # evita contar a mesma arvore mais de uma vez
 
 
-def forest_fire_portrayal(tree):
-    if tree is None:
-        return
-    portrayal = {"Shape": "rect", "w": 1, "h": 1, "Filled": "true", "Layer": 0}
-    (x, y) = tree.pos
-    portrayal["x"] = x
-    portrayal["y"] = y
-    portrayal["Color"] = COLORS[tree.condition]
-    return portrayal
+    dataframe_last_row = model.datacollector.get_model_vars_dataframe().tail(1)
+    n_fine_tree = dataframe_last_row.iloc[:, 0].tolist()
+
+    index = count/n_fine_tree[0] # index: das arvores "Fine" quais sobreviverao a fogo direto?
+
+    return index
 
 
-canvas_element = CanvasGrid(forest_fire_portrayal, 100, 100, 500, 500)
-tree_chart = ChartModule(
-    [{"Label": label, "Color": color} for (label, color) in COLORS.items()]
-)
-pie_chart = PieChartModule(
-    [{"Label": label, "Color": color} for (label, color) in COLORS.items()]
-)
+class ForestFire(Model):
+    """
+    Simple Forest Fire model.
+    """
 
-model_params = {
-    "height": 100,
-    "width": 100,
-    "density": UserSettableParameter("slider", "Tree density", 0.65, 0.01, 1.0, 0.01),
-    # paramentro resistencia da arvore ao fogo (casca)
-    "skin": UserSettableParameter("slider", "Tree skin thickness", 5.0, 0.0, 10.0, 1.0)
-}
-server = ModularServer(
-    ForestFire, [canvas_element, tree_chart, pie_chart], "Forest Fire", model_params
-)
+    def __init__(self, width=100, height=100, density=0.65, skin=5.0, firefighters=50):
+        """
+        Create a new forest fire model.
+
+        Args:
+            width, height: The size of the grid to model
+            density: What fraction of grid cells have a tree in them.
+        """
+        # Set up model objects
+        self.schedule = RandomActivation(self)
+        self.grid = Grid(width, height, torus=False)
+
+        self.datacollector = DataCollector(
+            {
+                "Fine": lambda m: self.count_type(m, "Fine"),
+                "On Fire": lambda m: self.count_type(m, "On Fire"),
+                "Burned Out": lambda m: self.count_type(m, "Burned Out"),
+                "Firefighters": lambda m: self.count_type(m, "Firefighters"),   # TODO: como isso funciona?
+            }
+        )
+
+        # Place a tree in each cell with Prob = density
+        for (contents, x, y) in self.grid.coord_iter():
+            if self.random.random() < density:
+                # Create a tree
+                new_tree = TreeCell((x, y), self, skin, firefighters) # arvore tem parametro "skin" e "firefighters"
+                # Set all trees in the first column on fire.
+                if x == 0:
+                    new_tree.condition = "On Fire"
+                self.grid._place_agent((x, y), new_tree)
+                self.schedule.add(new_tree)
+
+        self.running = True
+        self.datacollector.collect(self)
+
+    def step(self):
+        """
+        Advance the model by one step.
+        """
+        self.schedule.step()
+        # collect data
+        self.datacollector.collect(self)
+
+        # Halt if no more fire or firefighters arrived
+        if self.count_type(self, "On Fire") == 0 or self.count_type(self, "Firefighters") > 0:
+            self.running = False
+            # dataframe para csv variaveis de agente
+            self.datacollector.get_model_vars_dataframe().to_csv('agent_var.csv')
+            # variavel de modelo
+            res_index = shield_trees_index(self)
+
+            fields = ['Shield Index']
+            row = [res_index]
+
+            # criando csv 
+            with open('model_var.csv', 'w') as csvfile: 
+                # writer object
+                csvwriter = csv.writer(csvfile) 
+                    
+                # campos 
+                csvwriter.writerow(fields) 
+                    
+                # linhas 
+                csvwriter.writerow(row)
+
+    @staticmethod
+    def count_type(model, tree_condition):
+        """
+        Helper method to count trees in a given condition in a given model.
+        """
+        count = 0
+        for tree in model.schedule.agents:
+            if tree.condition == tree_condition:
+                count += 1
+        return count
